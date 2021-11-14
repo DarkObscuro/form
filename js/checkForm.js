2093915b68c0e154a8543d6c4a9c6f51c96af561
Nicolas <nicolas_thiphaine@yahoo.fr>
2021-11-14 15:53:28 +0300

    Form 2.0

--- js/checkForm.js
+++ js/checkForm.js
@@ -3,16 +3,12 @@ var checkFormFields1 = function() {
     var last_name = document.getElementById('lastname');
     var email = document.getElementById('email'); 
     var street_address = document.getElementById('streetaddress');
-    var address_2 = document.getElementById('address2'); 
     var city =  document.getElementById('city');
-    var region = document.getElementById('region'); 
-    var postal = document.getElementById('postal');
-    var country = document.getElementById('country'); 
     var citizenship = document.getElementById('citizenship');
     var dob = document.getElementById('dob'); 
     var mobile_phone = document.getElementById('mobilephone');
     var comments = document.getElementById('comments');
-    var check = first_name.value && last_name.value && email.value && street_address.value && city.value && region.value && postal.value && country.value && citizenship.value && dob.value && mobile_phone.value;
+    var check = first_name.value && last_name.value && email.value && street_address.value && city.value && citizenship.value && dob.value && mobile_phone.value;
     
     if (check) {
         comments.innerHTML = '';
@@ -25,35 +21,9 @@ var checkFormFields1 = function() {
 var checkFormFields2 = function() {
     var employer = document.getElementById('employer'); 
     var job_title = document.getElementById('jobtitle');
-    var division = document.getElementById('division'); 
-    var work_street_address = document.getElementById('workstreetaddress');
-    var work_address_2 = document.getElementById('workaddress2'); 
-    var work_city = document.getElementById('workcity');
-    var work_state = document.getElementById('workstate'); 
-    var work_postal = document.getElementById('workpostal');
-    var work_country = document.getElementById('workcountry');
-    var work_mobile_phone = document.getElementById('workmobilephone'); 
-    var work_email = document.getElementById('workemail');
+    var division = document.getElementById('division');
     var comments = document.getElementById('comments');
-    var check = employer.value && job_title.value && division.value && work_street_address.value && work_city.value && work_state.value && work_postal.value && work_country.value && work_mobile_phone.value && work_email.value;
-
-    if (check) {
-        comments.innerHTML = '';
-    } else {
-        comments.innerHTML = 'Please fill in all the <span style="color: #ff0000">required fields</span>';
-    }
-    return check;
-}
-
-var checkFormFields3 = function() {
-    var uni_1 = document.getElementById('uni1'); 
-    var degree_1 = document.getElementById('degree1');
-    var month_from_1 = document.getElementById('monthfrom1'); 
-    var year_from_1 =  document.getElementById('yearfrom1');
-    var month_to_1 = document.getElementById('monthto1'); 
-    var year_to_1 = document.getElementById('yearto1');
-    var comments = document.getElementById('comments');
-    var check = uni_1.value && degree_1.value && month_from_1.value && year_from_1.value && month_to_1.value && year_to_1.value;
+    var check = employer.value && job_title.value && division.value;
 
     if (check) {
         comments.innerHTML = '';
--- php/insert.php
+++ php/insert.php
@@ -34,85 +34,33 @@
                 }
             }
             
-            $stmt = $conn->prepare("INSERT INTO trainees (id,title,first_name,last_name,preferred_name,email,street_address,address_2,city,region,postal,country,citizenship,dob,home_phone,mobile_phone,inter_phone,employer,job_title,division,work_street_address,work_address_2,work_city,work_state,work_postal,work_country,work_home_phone,work_mobile_phone,work_email,uni_1,degree_1,month_from_1,year_from_1,month_to_1,year_to_1,uni_2,degree_2,month_from_2,year_from_2,month_to_2,year_to_2,uni_3,degree_3,month_from_3,year_from_3,month_to_3,year_to_3,hear,conference,otherC,experience,why,cv,NPO,special,pay,bill_first_name,bill_last_name,bill_email,bill_street_address,bill_address_2,bill_city,bill_state,bill_postal,bill_country) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+            $stmt = $conn->prepare("INSERT INTO trainees (id,first_name,last_name,email,street_address,city,citizenship,dob,mobile_phone,employer,job_title,division,hear,conference,otherC,why,special) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 
-            $stmt->bind_param("sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",$id,$title,$first_name,$last_name,$preferred_name,$email,$street_address,$address_2,$city,$region,$postal,$country,$citizenship,$dob,$home_phone,$mobile_phone,$inter_phone,$employer,$job_title,$division,$work_street_address,$work_address_2,$work_city,$work_state,$work_postal,$work_country,$work_home_phone,$work_mobile_phone,$work_email,$uni_1,$degree_1,$month_from_1,$year_from_1,$month_to_1,$year_to_1,$uni_2,$degree_2,$month_from_2,$year_from_2,$month_to_2,$year_to_2,$uni_3,$degree_3,$month_from_3,$year_from_3,$month_to_3,$year_to_3,$hear,$conference,$otherC,$experience,$why,$cv,$NPO,$special,$pay,$bill_first_name,$bill_last_name,$bill_email,$bill_street_address,$bill_address_2,$bill_city,$bill_state,$bill_postal,$bill_country);
+            $stmt->bind_param("sssssssssssssssss",$id,$first_name,$last_name,$email,$street_address,$city,$citizenship,$dob,$mobile_phone,$employer,$job_title,$division,$hear,$conference,$otherC,$why,$special);
 
             // Taking all values from the form data
             $id = "";
             // Page 1
-            $title = checkEmpty($conn,'title');
             $first_name = checkEmpty($conn,'firstname'); 
             $last_name = checkEmpty($conn,'lastname');
-            $preferred_name = checkEmpty($conn,'preferredname');
             $email = checkEmpty($conn,'email'); 
             $street_address = checkEmpty($conn,'streetaddress');
-            $address_2 = checkEmpty($conn,'address2'); 
             $city = checkEmpty($conn,'city');
-            $region = checkEmpty($conn,'region'); 
-            $postal = checkEmpty($conn,'postal');
-            $country = checkEmpty($conn,'country'); 
             $citizenship = checkEmpty($conn,'citizenship');
             $dob = checkEmpty($conn,'dob'); 
-            $home_phone = checkEmpty($conn,'homephone');
-            $mobile_phone = checkEmpty($conn,'mobilephone'); 
-            $inter_phone = checkEmpty($conn,'interphone');
+            $mobile_phone = checkEmpty($conn,'mobilephone');
 
             // Page 2
             $employer = checkEmpty($conn,'employer'); 
             $job_title = checkEmpty($conn,'jobtitle');
             $division = checkEmpty($conn,'division'); 
-            $work_street_address = checkEmpty($conn,'workstreetaddress');
-            $work_address_2 = checkEmpty($conn,'workaddress2'); 
-            $work_city = checkEmpty($conn,'workcity');
-            $work_state = checkEmpty($conn,'workstate'); 
-            $work_postal = checkEmpty($conn,'workpostal');
-            $work_country = checkEmpty($conn,'workcountry'); 
-            $work_home_phone = checkEmpty($conn,'workhomephone');
-            $work_mobile_phone = checkEmpty($conn,'workmobilephone'); 
-            $work_email = checkEmpty($conn,'workemail');
-
-            // Page 3
-            $uni_1 = checkEmpty($conn,'uni1'); 
-            $degree_1 = checkEmpty($conn,'degree1');
-            $month_from_1 = checkEmpty($conn,'monthfrom1'); 
-            $year_from_1 = checkEmpty($conn,'yearfrom1');
-            $month_to_1 = checkEmpty($conn,'monthto1'); 
-            $year_to_1 = checkEmpty($conn,'yearto1');
-            $uni_2 = checkEmpty($conn,'uni2');
-            $degree_2 = checkEmpty($conn,'degree2');
-            $month_from_2 = checkEmpty($conn,'monthfrom2');
-            $year_from_2 = checkEmpty($conn,'yearfrom2');
-            $month_to_2 = checkEmpty($conn,'monthto2');
-            $year_to_2 = checkEmpty($conn,'yearto2');
-            $uni_3 = checkEmpty($conn,'uni3');
-            $degree_3 = checkEmpty($conn,'degree3');
-            $month_from_3 = checkEmpty($conn,'monthfrom3');
-            $year_from_3 = checkEmpty($conn,'yearfrom3');
-            $month_to_3 = checkEmpty($conn,'monthto3');
-            $year_to_3 = checkEmpty($conn,'yearto3');
-
+            
             // Page 4
             $hear = checkEmpty($conn,'hear');
             $conference = checkEmpty($conn,'conference');
             $otherC = checkEmpty($conn,'otherC');
-            $experience = checkEmpty($conn,'experience');
             $why = checkEmpty($conn,'why');
-            $cv = checkEmpty($conn,'cv');
-            $NPO = checkEmpty($conn,'NPO');
             $special = checkEmpty($conn,'special');
-
-            // Page 5
-            $pay = checkEmpty($conn,'pay');
-            $bill_first_name = checkEmpty($conn,'billfirstname');
-            $bill_last_name = checkEmpty($conn,'billlastname');
-            $bill_email = checkEmpty($conn,'billemail');
-            $bill_street_address = checkEmpty($conn,'billstreetaddress');
-            $bill_address_2 = checkEmpty($conn,'billaddress2');
-            $bill_city = checkEmpty($conn,'billcity');
-            $bill_state = checkEmpty($conn,'billstate');
-            $bill_postal = checkEmpty($conn,'billpostal');
-            $bill_country = checkEmpty($conn,'billcountry');
 
             $stmt->execute();
             echo "<h3>Your registration is complete.</h3>";
