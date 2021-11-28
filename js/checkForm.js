4a9cf442736673f57cf3239f456a014a7c069eb9
Nicolas <nicolas_thiphaine@yahoo.fr>
2021-11-28 16:55:25 +0400

    mail validation js

--- js/checkForm.js
+++ js/checkForm.js
@@ -1,14 +1,14 @@
 var checkFormFields1 = function() {
     var first_name = document.getElementById('firstname'); 
     var last_name = document.getElementById('lastname');
-    var email = document.getElementById('email'); 
+    var emailcheck = ValidateEmail(); 
     var street_address = document.getElementById('streetaddress');
     var city =  document.getElementById('city');
     var citizenship = document.getElementById('citizenship');
     var dob = document.getElementById('dob'); 
     var mobile_phone = document.getElementById('mobilephone');
     var comments = document.getElementById('comments');
-    var check = first_name.value && last_name.value && email.value && street_address.value && city.value && citizenship.value && dob.value && mobile_phone.value;
+    var check = first_name.value && last_name.value && emailcheck && street_address.value && city.value && citizenship.value && dob.value && mobile_phone.value;
     
     if (check) {
         comments.innerHTML = '';
@@ -31,4 +31,17 @@ var checkFormFields2 = function() {
         comments.innerHTML = 'Please fill in all the <span style="color: #ff0000">required fields</span>';
     }
     return check;
+}
+
+function ValidateEmail() 
+{
+    var elem = document.getElementById("email");
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (elem.value.match(mailformat))
+    {
+        return (true);
+    } else {
+        alert("You have entered an invalid email address!");
+        return (false);
+    }
 }
